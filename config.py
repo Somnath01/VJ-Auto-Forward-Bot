dffcb4c5d9a98a49a2a3b4651f35d9cf5c5469d5
HACKER BOY <54860406+Somnath01@users.noreply.github.com>
2024-01-07 15:23:05 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ class Config(object):
       API_HASH = getenv("API_HASH", "ce968070d72fe013582d41e4a1e7bee4")
       API_ID = int(getenv("API_ID", "22636431"))
       AS_COPY = True if getenv("AS_COPY", True) == "`{file_name}`" else True
-      BOT_TOKEN = getenv("BOT_TOKEN", "mongodb+srv://shizuroy1998:6ZR2SMmIS5tlqJQw@cluster0.i5hwgxd.mongodb.net/?retryWrites=true&w=majority")
+      BOT_TOKEN = getenv("BOT_TOKEN", "6433801313:AAGFA3VPt4dUOp28GH4ZFyrHl0zGpavfkcQ")
       CHANNEL = list(x for x in getenv("CHANNEL_ID", "-1001795733255:-1001999990315").replace("\n", " ").split(' '))
 
 
